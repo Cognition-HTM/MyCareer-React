72e0953bd7ee1471fff1e3d5291dcc19c35a812a
RahulSharma4329 <rahulsharma4329@gmail.com>
2022-11-15 19:45:57 +0530

    Added the quiz component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Nav from "./Components/Nav"
-import Login from "./Pages/Login"
-import Signup from "./Pages/Signup"
+import Nav from "./Components/Nav";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Quiz from "./Components/Student/Quiz";
 export default function App() {
   return (
     <>
-    <Nav/>
+      <Nav />
+      <Quiz />
     </>
-
-  )
+  );
 }
